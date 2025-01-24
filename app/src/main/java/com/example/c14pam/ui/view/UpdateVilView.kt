9349955b3ddbc0aba363f20e617b482931e20c60
mrendy58 <derendyyy58@gmail.com>
2025-01-24 20:31:03 +0700

    membuat fungsi FormUpdate untuk text field input data

---
+++ app/src/main/java/com/example/c14pam/ui/view/UpdateVilView.kt
@@ -0,0 +1,72 @@
+package com.example.c14pam.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.c14pam.ui.navigation.DestinasiNavigasi
+import com.example.c14pam.ui.viewmodel.InsertVilUiEvent
+
+
+object DestinasiVilUpdate : DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Villa"
+    const val ID_VILLA = "id_villa"
+    val routesWithArg = "$route/{$ID_VILLA}"
+}
+
+
+
+@Composable
+fun FormUpdate(
+    insertVilUiEvent: InsertVilUiEvent,
+    onValueChange: (InsertVilUiEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        // Input ID Villa
+        OutlinedTextField(
+            value = insertVilUiEvent.id_villa,
+            onValueChange = { onValueChange(insertVilUiEvent.copy(id_villa = it)) },
+            label = { Text("ID Villa") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        // Input Nama Villa
+        OutlinedTextField(
+            value = insertVilUiEvent.nama_villa,
+            onValueChange = { onValueChange(insertVilUiEvent.copy(nama_villa = it)) },
+            label = { Text("Nama Villa") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        // Input Alamat
+        OutlinedTextField(
+            value = insertVilUiEvent.alamat,
+            onValueChange = { onValueChange(insertVilUiEvent.copy(alamat = it)) },
+            label = { Text("Alamat") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        // Input Kamar Tersedia
+        OutlinedTextField(
+            value = insertVilUiEvent.kamar_tersedia,
+            onValueChange = { onValueChange(insertVilUiEvent.copy(kamar_tersedia = it)) },
+            label = { Text("Kamar Tersedia") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true
+        )
+    }
+}
