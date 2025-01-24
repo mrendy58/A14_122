e24401c4d7e784de49075ce0a174d2c14d2c1dfe
mrendy58 <derendyyy58@gmail.com>
2025-01-24 17:05:00 +0700

    membuat RsvVillaApp untuk mengatur struktur applikasi

---
+++ app/src/main/java/com/example/c14pam/RsvVillaApp.kt
@@ -0,0 +1,15 @@
+package com.example.c14pam
+
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.c14pam.ui.navigation.PengelolaHalaman
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RsvVillaApp(modifier: Modifier = Modifier) {
+
+
+            PengelolaHalaman()
+        }
