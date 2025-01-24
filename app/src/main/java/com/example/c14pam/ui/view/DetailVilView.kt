c4d798fa5b297a2f4f7a0961f3eea5ce6fcf5147
mrendy58 <derendyyy58@gmail.com>
2025-01-24 20:43:20 +0700

    membuat fungsi untuk menampilkan detail dalam bentuk card

--- app/src/main/java/com/example/c14pam/ui/view/DetailVilView.kt
+++ app/src/main/java/com/example/c14pam/ui/view/DetailVilView.kt
@@ -7,7 +7,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.c14pam.model.Villa
 import com.example.c14pam.ui.navigation.DestinasiNavigasi
 
 
@@ -18,7 +20,33 @@ object DestinasiVilDetail : DestinasiNavigasi {
     val routeWithArg = "$route/{$ID_VILLA}"
 }
 
-
+@Composable
+fun ItemDetailVla(
+    modifier: Modifier = Modifier,
+    villa: Villa
+) {
+    Card(
+        modifier = modifier.fillMaxWidth().padding(top = 20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White, // Warna latar belakang kartu putih
+            contentColor = Color.Black // Warna teks hitam
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            ComponentDetailVla(judul = "Id Villa", isi = villa.id_villa, titleColor = Color(0xFF2196F3)) // Warna biru
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailVla(judul = "Nama Villa", isi = villa.nama_villa, titleColor = Color(0xFF2196F3)) // Warna biru
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailVla(judul = "Alamat", isi = villa.alamat, titleColor = Color(0xFF2196F3)) // Warna biru
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailVla(judul = "Kamar Tersedia", isi = villa.kamar_tersedia, titleColor = Color(0xFF2196F3)) // Warna biru
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailVla(
