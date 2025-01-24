f1511e9ef84da02da22b195d3c761c144dfc26fc
mrendy58 <derendyyy58@gmail.com>
2025-01-24 22:54:32 +0700

    menambahkan data baru untuk reservasi

--- app/src/main/java/com/example/c14pam/model/RsvVilla.kt
+++ app/src/main/java/com/example/c14pam/model/RsvVilla.kt
@@ -9,3 +9,13 @@ data class Villa(
     val alamat: String,
     val kamar_tersedia: String
 )
+
+@Serializable
+data class Reservasi(
+    val id_reservasi: String,
+    val id_villa: String,
+    val id_pelanggan: String,
+    val check_in: String,
+    val check_out: String,
+    val jumlah_kamar: String
+)
