f4c09a0bf7819ecb94abc31821f9f36bb3c9a693
mrendy58 <derendyyy58@gmail.com>
2025-01-24 16:24:17 +0700

    membuat appcontainer untuk menyediakan dependency

---
+++ app/src/main/java/com/example/c14pam/repository/RsvVillaContainer.kt
@@ -0,0 +1,41 @@
+package com.example.c14pam.repository
+
+import com.example.c14pam.service.VillaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+// Interface AppContainer digunakan untuk menyediakan dependensi yang diperlukan oleh aplikasi.
+// Ini adalah bagian dari pola Dependency Injection (DI) untuk memisahkan pembuatan objek dari penggunaannya.
+interface AppContainer {
+    val villaRepository: VillaRepository // Properti untuk menyediakan instance VillaRepository
+}
+
+// Implementasi dari AppContainer yang menyediakan instance VillaRepository.
+class RsvVillaContainer : AppContainer {
+    // Base URL untuk API. Dalam hal ini, menggunakan emulator Android (10.0.2.2 adalah localhost).
+    private val baseurl = "http://10.0.2.2:82/Booking/"
+
+    // Konfigurasi JSON untuk parsing data dari API.
+    // ignoreUnknownKeys = true memungkinkan untuk mengabaikan field yang tidak dikenal dalam JSON.
+    private val json = Json { ignoreUnknownKeys = true }
+
+    // Membuat instance Retrofit untuk melakukan HTTP request ke API.
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl) // Mengatur base URL untuk API.
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType())) // Menggunakan Kotlinx.serialization sebagai converter.
+        .build()
+
+    // Lazy initialization untuk VillaService.
+    // VillaService adalah interface yang mendefinisikan endpoint API.
+    private val villaService: VillaService by lazy {
+        retrofit.create(VillaService::class.java) // Membuat instance VillaService menggunakan Retrofit.
+    }
+
+    // Lazy initialization untuk VillaRepository.
+    // NetworkVillaRepository adalah implementasi dari VillaRepository yang menggunakan VillaService.
+    override val villaRepository: VillaRepository by lazy {
+        NetworkVillaRepository(villaService) // Membuat instance NetworkVillaRepository dengan VillaService.
+    }
+}
